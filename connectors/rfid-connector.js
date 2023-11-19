2d606f048708e03e3ef7bd5ddce18b667396f172
Talon Grayson <talongrayson@gmail.com>
2023-11-19 06:36:35 +0000

    Fix bug

--- connectors/rfid-connector.js
+++ connectors/rfid-connector.js
@@ -2,6 +2,10 @@ const Mfrc522 = require("mfrc522-rpi");
 const SoftSPI = require("rpi-softspi");
 const { rfidEventListener } = require("../actions/actions-listener");
 
-export default listenForTags = () => {
+listenForTags = () => {
     rfidEventListener();
 }
+
+module.exports = {
+    listenForTags
+  };
