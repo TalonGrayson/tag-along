a93200e7ef1d7186cb93fdb4f403c68096742201
Talon Grayson <talongrayson@gmail.com>
2023-11-19 05:55:13 +0000

    Rudimentary db

--- actions/actions-listener.js
+++ actions/actions-listener.js
@@ -41,43 +41,27 @@ rfidScanListener = () => {
 
     //# Scan for cards
     let response = mfrc522.findCard();
-    if (!response.status) {
-      console.log("No Card");
-      return;
-    }
-    console.log("Card detected, CardType: " + response.bitSize);
+    if (!response.status) return;
 
     //# Get the UID of the card
     response = mfrc522.getUid();
-    if (!response.status) {
+    if (response.status) {
+      const rfidUid = parsedRfidTag(response.data);
+    } else {
       console.log("UID Scan Error");
       return;
     }
 
-    //# If we have the UID, continue
-    const uid = response.data;
-    console.log("Response: %o", response);
-    console.log("Uid: %o", mfrc522.getUid());
-    console.log("Card read UID: %o", parsedRfidTag(uid));
-
-    //# Select the scanned card
-    const memoryCapacity = mfrc522.selectCard(uid);
-    console.log("Card Memory Capacity: " + memoryCapacity);
-
-    //# This is the default key for authentication
-    const key = [0xff, 0xff, 0xff, 0xff, 0xff, 0xff];
-
-    //# Authenticate on Block 8 with key and uid
-    if (!mfrc522.authenticate(8, key, uid)) {
-      console.log("Authentication Error");
-      return;
+    switch(rfidUid) {
+      case "4:28:82:26":
+        console.log("Mega Man");
+      case "4:1f:c5:56":
+        console.log("Jack Sparrow");
+      case "4:6b:2e:c9":
+        console.log("Talon Grayson");
+      default:
+        console.log("Unknown tag");
     }
-
-    //# Dump Block 8
-    console.log("Block: 8 Data: " + mfrc522.getDataForBlock(8));
-
-    //# Stop
-    mfrc522.stopCrypto();
   }, 500);
 
 }
