0bf38f038808aa92cac1ca8540faf93bca58ed98
Talon Grayson <talongrayson@gmail.com>
2023-11-19 05:33:09 +0000

    Parse RFID tag IDs

--- actions/actions-listener.js
+++ actions/actions-listener.js
@@ -26,10 +26,6 @@ rfidScanListener = () => {
   "use strict";
 
   //# This loop keeps checking for chips. If one is near it will get the UID and authenticate
-  console.log("scanning...");
-  console.log("Please put chip or keycard in the antenna inductive zone!");
-  console.log("Press Ctrl-C to stop.");
-
   const softSPI = new SoftSPI({
     clock: 23, // pin number of SCLK
     mosi: 19, // pin number of MOSI
@@ -37,8 +33,6 @@ rfidScanListener = () => {
     client: 24 // pin number of CS
   });
 
-  // GPIO 24 can be used for buzzer bin (PIN 18), Reset pin is (PIN 22).
-  // I believe that channing pattern is better for configuring pins which are optional methods to use.
   const mfrc522 = new Mfrc522(softSPI).setResetPin(22)
 
   setInterval(function() {
@@ -59,16 +53,10 @@ rfidScanListener = () => {
       console.log("UID Scan Error");
       return;
     }
-    
+
     //# If we have the UID, continue
     const uid = response.data;
-    console.log(
-      "Card read UID: %s %s %s %s",
-      uid[0].toString(16),
-      uid[1].toString(16),
-      uid[2].toString(16),
-      uid[3].toString(16)
-    );
+    console.log("Card read UID: %o", parsedRfidTag(uid));
 
     //# Select the scanned card
     const memoryCapacity = mfrc522.selectCard(uid);
@@ -90,6 +78,10 @@ rfidScanListener = () => {
     mfrc522.stopCrypto();
   }, 500);
 
+}
+
+parsedRfidTag = (uid) => {
+  `${uid[1].toString(16)}:${uid[2].toString(16)}:${uid[3].toString(16)}:${uid[4].toString(16)}:${uid[5].toString(16)}:${uid[6].toString(16)}:${uid[7].toString(16)}:`
 }
 
 module.exports = {
