e3006bf209b02f91594820d72decea6eb72576ae
Talon Grayson <talongrayson@gmail.com>
2023-11-20 18:46:56 +0000

    Read OBS reidentify error message

--- connectors/obs-connector.js
+++ connectors/obs-connector.js
@@ -20,7 +20,7 @@ obs
     // Promise convention dicates you have a catch on every chain.
     console.log({ obs_websocket_error: err });
     console.log("Reidentifying...");
-    obs.reidentify().catch(error => console.error("Error: %o", error));
+    obs.reidentify().catch(error => console.error("Error: %o", error.message));
   });
 
 // You must add this handler to avoid uncaught exceptions.
